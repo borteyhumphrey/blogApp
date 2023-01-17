2e8c00bfb6128b2fa82175d7ec75a9d2924912b4
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-17 22:43:19 +0000

    Update posts model

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -11,12 +11,16 @@ class Posts(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
+    status = models.CharField(max_length=2,
+                             choices=Status.choices,
+                             default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
