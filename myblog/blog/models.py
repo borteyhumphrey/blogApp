d959dfbeb9632173e8d0fdd3acef87e292ebbdfa
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-19 22:18:34 +0000

    Rename posts model to post

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
