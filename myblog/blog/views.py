8a0a371f40aa2635ab0a2d7a8fad8bf3617117d8
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-21 20:06:00 +0000

    Define list and detail views

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = "blog/post/list.html"
+
+
+class PostDetailView(DetailView):
+    queryset = Post.published.all()
+    template_name = "blog/post/detail.html"
