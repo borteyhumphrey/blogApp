4f32eb7e459c2bafde1b57d798798b285a1f156b
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-01-29 00:15:36 +0000

    Change posts per page

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 4
+    paginate_by = 3
     template_name = "blog/post/list.html"
 
 def post_detail(request, year, month, day, post):
